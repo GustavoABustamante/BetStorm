2c3f2c9dc394231033a20608573747edfc861d28
GUSTAVO ADOLFO BUSTAMANTE BUSTAMANTE <gustavobustamante1996@gmail.com>
2022-02-05 21:43:58 -0300

    Inicio proyecto BetStorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderSectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
